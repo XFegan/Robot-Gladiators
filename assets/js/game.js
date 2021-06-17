6d7778128738be033413b3e10cda348410fb9cfb
XFegan <xavierslf11@gmail.com>
2021-06-16 21:14:55 -0400

    testing update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,5 @@
-window.alert("This is an alert! JavaScript is running!");
-
 // this creates a function named "fight"
 
-function fight(); {
-    window.alert("The fight has begun!");
-  }
+function fight() {
+  window.alert("The fight has begun!");
+}
